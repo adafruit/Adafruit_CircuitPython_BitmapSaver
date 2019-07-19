7cb0c6164745ea7e9f3ed0b478807a2ddfef4e2e
Dave Astels <dastels@daveastels.com>
2019-07-19 17:46:40 -0400

    Revert "Revert "Move code over""
    This reverts commit b186fdb07b5f14bf75895546cd4f965c0e3efb5a.

---
+++ adafruit_bitmapsaver.py
@@ -0,0 +1,117 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Dave Astels for Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+`adafruit_bitmapsaver`
+================================================================================
+
+Save a displayio.Bitmap (and associated displayio.Palette) into a BMP file.
+
+
+* Author(s): Dave Astels
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://github.com/adafruit/circuitpython/releases
+
+"""
+
+# imports
+
+import struct
+from displayio import Bitmap, Palette
+
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BitmapSaver.git"
+
+#pylint:disable=line-too-long,broad-except,redefined-outer-name
+
+def _write_bmp_header(output_file, filesize):
+    output_file.write(bytes('BM', 'ascii'))
+    output_file.write(struct.pack('<I', filesize))
+    output_file.write(b'\00\x00')
+    output_file.write(b'\00\x00')
+    output_file.write(struct.pack('<I', 54))
+
+def _write_dib_header(output_file, bitmap):
+    output_file.write(struct.pack('<I', 40))
+    output_file.write(struct.pack('<I', bitmap.width))
+    output_file.write(struct.pack('<I', bitmap.height))
+    output_file.write(struct.pack('<H', 1))
+    output_file.write(struct.pack('<H', 24))
+    output_file.write(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+
+def _swap_bytes(value):
+    return ((value & 0xFF00) >> 8) | ((value & 0x00FF) << 8)
+
+def _bytes_per_row(bitmap):
+    pixel_bytes = 3 * bitmap.width
+    padding_bytes = (4 - (pixel_bytes % 4)) % 4
+    return pixel_bytes + padding_bytes
+
+def _write_pixels(output_file, bitmap, palette):
+    row_buffer = bytearray(_bytes_per_row(bitmap))
+
+    for y in range(bitmap.height, 0, -1):
+        buffer_index = 0
+        for x in range(bitmap.width):
+            pixel = bitmap[x, y-1]             # this is an index into the palette
+            color = _swap_bytes(palette[pixel])  # This is a 16 bit value in r5g6b5 format
+            blue = (color << 3) & 0x00F8    # extract each of the RGB tripple into it's own byte
+            green = (color >> 3) & 0x00FC
+            red = (color >> 8) & 0x00F8
+            for value in (blue, green, red):
+                row_buffer[buffer_index] = value
+                buffer_index += 1
+        output_file.write(row_buffer)
+
+def save_bitmap(bitmap, palette, file_or_filename):
+    """Save a bitmap (using an associated palette) to a 24 bit per pixel BMP file.
+
+    :param bitmap: the displayio.Bitmap to save
+    :param palette: the displayio.Palette to use for looking up colors in the bitmap
+    """
+    if not isinstance(bitmap, Bitmap):
+        raise ValueError('bitmap')
+    if not isinstance(palette, Palette):
+        raise ValueError('palette')
+    try:
+        if isinstance(file_or_filename, str):
+            output_file = open(file_or_filename, 'wb')
+        else:
+            output_file = file_or_filename
+
+        filesize = 54 + bitmap.height * _bytes_per_row(bitmap)
+        _write_bmp_header(output_file, filesize)
+        _write_dib_header(output_file, bitmap)
+        _write_pixels(output_file, bitmap, palette)
+    except Exception:
+        print('Error saving bitmap')
+        raise
+    else:
+        output_file.close()
