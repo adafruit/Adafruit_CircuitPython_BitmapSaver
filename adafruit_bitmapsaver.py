e6e827bab42c0d1df57c7e73742b0b06c3d80cf1
dherrada <dylan.herrada@gmail.com>
2020-03-09 16:50:39 -0400

    Ran black, made pylint 2.x compatible

--- adafruit_bitmapsaver.py
+++ adafruit_bitmapsaver.py
@@ -54,25 +54,28 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BitmapSaver.git"
 
 
 def _write_bmp_header(output_file, filesize):
-    output_file.write(bytes('BM', 'ascii'))
-    output_file.write(struct.pack('<I', filesize))
-    output_file.write(b'\00\x00')
-    output_file.write(b'\00\x00')
-    output_file.write(struct.pack('<I', 54))
+    output_file.write(bytes("BM", "ascii"))
+    output_file.write(struct.pack("<I", filesize))
+    output_file.write(b"\00\x00")
+    output_file.write(b"\00\x00")
+    output_file.write(struct.pack("<I", 54))
+
 
 def _write_dib_header(output_file, width, height):
-    output_file.write(struct.pack('<I', 40))
-    output_file.write(struct.pack('<I', width))
-    output_file.write(struct.pack('<I', height))
-    output_file.write(struct.pack('<H', 1))
-    output_file.write(struct.pack('<H', 24))
+    output_file.write(struct.pack("<I", 40))
+    output_file.write(struct.pack("<I", width))
+    output_file.write(struct.pack("<I", height))
+    output_file.write(struct.pack("<H", 1))
+    output_file.write(struct.pack("<H", 24))
     for _ in range(24):
-        output_file.write(b'\x00')
+        output_file.write(b"\x00")
+
 
 def _bytes_per_row(source_width):
     pixel_bytes = 3 * source_width
     padding_bytes = (4 - (pixel_bytes % 4)) % 4
     return pixel_bytes + padding_bytes
+
 
 def _rotated_height_and_width(pixel_source):
     # flip axis if the display is rotated
@@ -80,13 +83,15 @@ def _rotated_height_and_width(pixel_source):
         return (pixel_source.height, pixel_source.width)
     return (pixel_source.width, pixel_source.height)
 
+
 def _rgb565_to_bgr_tuple(color):
-    blue = (color << 3) & 0x00F8    # extract each of the RGB tripple into it's own byte
+    blue = (color << 3) & 0x00F8  # extract each of the RGB tripple into it's own byte
     green = (color >> 3) & 0x00FC
     red = (color >> 8) & 0x00F8
     return (blue, green, red)
 
-#pylint:disable=too-many-locals
+
+# pylint:disable=too-many-locals
 def _write_pixels(output_file, pixel_source, palette):
     saving_bitmap = isinstance(pixel_source, Bitmap)
     width, height = _rotated_height_and_width(pixel_source)
@@ -96,14 +101,14 @@ def _write_pixels(output_file, pixel_source, palette):
         buffer_index = 0
         if saving_bitmap:
             for x in range(width):
-                pixel = pixel_source[x, y-1]
+                pixel = pixel_source[x, y - 1]
                 color = palette[pixel]
                 for _ in range(3):
                     row_buffer[buffer_index] = color & 0xFF
                     color >>= 8
                     buffer_index += 1
         else:
-            data = pixel_source.fill_row(y-1, result_buffer)
+            data = pixel_source.fill_row(y - 1, result_buffer)
             for i in range(width):
                 pixel565 = (data[i * 2] << 8) + data[i * 2 + 1]
                 for b in _rgb565_to_bgr_tuple(pixel565):
@@ -111,7 +116,10 @@ def _write_pixels(output_file, pixel_source, palette):
                     buffer_index += 1
         output_file.write(row_buffer)
         gc.collect()
-#pylint:enable=too-many-locals
+
+
+# pylint:enable=too-many-locals
+
 
 def save_pixels(file_or_filename, pixel_source=board.DISPLAY, palette=None):
     """Save pixels to a 24 bit per pixel BMP file.
@@ -124,12 +132,12 @@ def save_pixels(file_or_filename, pixel_source=board.DISPLAY, palette=None):
     """
     if isinstance(pixel_source, Bitmap):
         if not isinstance(palette, Palette):
-            raise ValueError('Third argument must be a Palette for a Bitmap save')
+            raise ValueError("Third argument must be a Palette for a Bitmap save")
     elif not isinstance(pixel_source, Display):
-        raise ValueError('Second argument must be a Bitmap or Display')
+        raise ValueError("Second argument must be a Bitmap or Display")
     try:
         if isinstance(file_or_filename, str):
-            output_file = open(file_or_filename, 'wb')
+            output_file = open(file_or_filename, "wb")
         else:
             output_file = file_or_filename
 
